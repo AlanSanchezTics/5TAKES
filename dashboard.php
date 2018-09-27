56892ab5e2d66ed58a858b0068e125ba5041082f
Maria Gonzalez <elodia.wolff.satura@gmail.com>
2018-09-27 14:45:02 -0500

    cabecera y menu

--- dashboard.php
+++ dashboard.php
@@ -17,9 +17,6 @@
 </div>
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
