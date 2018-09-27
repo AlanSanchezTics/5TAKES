02525c7bcedab6cb45f964588c00e1c2ca737381
hectorjpalomino <33034249+hectorjpalomino@users.noreply.github.com>
2018-09-27 17:16:43 -0500

    s
    s

--- facturacion/buscarfolio.php
+++ facturacion/buscarfolio.php
@@ -1,8 +1,30 @@
 <?php
+include "../conexion.php";
+NuevoFolio( $_POST['folio']);
+
+function NuevoFolio($folio)
+{
+	$sentencia="SELECT *FROM tbl_facturacion WHERE tid=".$folio." and facturaexiste=1";
+        $user = mysqli_num_rows(mysqli_query($conexion,$sentencia));
+
+        if ($user>0)
+        {
+            echo "<script language='javascript'>"; 
+            echo "alert('el cobro Ya Esta Facturado');";
+            echo "window.location.href='vercobros.php';";
+            echo "</script>";
+
+
+        }else
+        {
+        }
+
+}
 
 
 
 
 
 
->
+
+?>
--- facturacion/vercobros.php
+++ facturacion/vercobros.php
@@ -4,10 +4,10 @@
 	<title>Cobros 5TAKES</title>
 </head>
 <body>
-<form>
+<form action="buscarfolio.php" method="post">
 <label>Ingrese Folio</label>
-<input type="text" id="folio"></input>	
-
+<input type="text" id="folio" name="folio"></input>	
+<input type="submit" value="Buscar Folio"></input>
 </form>
 
 </body>
