85741511d6712c6cdf1a4c10df3cee72dbe8453e
Maria Gonzalez <elodia.wolff.satura@gmail.com>
2018-09-27 15:36:16 -0500

    menu

--- dashboard.php
+++ dashboard.php
@@ -18,21 +18,17 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Page 1-1</a></li>
-          <li><a href="#">Page 1-2</a></li>
-          <li><a href="#">Page 1-3</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
+      <li><a href="#">Solicitudes</a></li>
+      <li><a href="#">Finanzas</a></li>
+      <li><a href="#">Proveedores</a></li>
+      <li><a href="#">Colaboradores</a></li>
+      <li><a href="#">Clientes</a></li>
+      <li><a href="#">Facturaci&oacute;n</a></li>
     </ul>
   </div>
 </nav>
 <div class="container">
-  <p>Contenido :3</p>      
+    
      
 </div>
 
