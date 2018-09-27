2a180f5ec95b413e399efebe36a5a36a827562a5
AlanSanchezTics <33034222+AlanSanchezTics@users.noreply.github.com>
2018-09-27 15:00:14 -0500

    terminando validacion de acceso

--- validacion.php
+++ validacion.php
@@ -1,6 +1,6 @@
 <?php
-    include "./conexion.php";
-
+include "./conexion.php";
+$array = array();
     /*$database = $firebase -> getDatabase();
 
     $newPost = $database
@@ -14,11 +14,27 @@
     $value = $reference ->getValue();
 
     echo(json_encode($value));*/
-        if(isset($_POST["user"]) && isset($_POST["password"])){
-            $user = $_POST["user"];
-            $clave = $_POST["password"];
+if (isset($_POST["user"]) && isset($_POST["password"])) {
+    $user = $_POST["user"];
+    $clave = $_POST["password"];
 
-        $sql="SELECT * FROM tbl_usuarios WHERE usuEmail='{$user}' AND usuContraseña=AES_DECRYPT({'$clave}','FIVETAKES')";
-        $result 
+    $sql = "SELECT * FROM tbl_usuarios WHERE UsuEmail='{$user}' AND UsuContraseña=AES_ENCRYPT('{$clave}','FIVETAKES')";
+    $result = mysqli_query($conexion, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        if ($reg = mysqli_fetch_array($result)) {
+            switch ($reg["UsuEstatus"]) {
+                case 0:
+                    echo "USUARIO INACTIVO";
+                    break;
+                case 1:
+                    echo "<script language='javascript'>";
+                    echo "window.location.href='dashboard.php'";
+                    echo "</script>";
+                default:
+                        # code...
+                    break;
+            }
         }
+    }
+}
 ?>
