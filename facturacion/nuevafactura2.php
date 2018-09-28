8924438e7f6adfaef549ace198a7b06a0e6bdb33
hectorjpalomino <33034249+hectorjpalomino@users.noreply.github.com>
2018-09-28 02:08:24 -0500

    f

--- facturacion/nuevafactura2.php
+++ facturacion/nuevafactura2.php
@@ -7,10 +7,19 @@ Nuevafactura($_POST['rfc'], $_POST['monto'], $_POST['rsocial'],$_POST['tpago'],$
 function Nuevafactura($rfc,$monto,$rsocial,$tpago,$direccion,$ciudad,$fecha,$iva,$tid)
 {
 	 include '../conexion.php';
+	 $cd=$rfc.$monto.$rsocial.$tpago.$direccion.$ciudad.$fecha.$iva.$tid;
+	 
+	 $cd=$cd.strrev($cd).$cd;
+
+	 var_dump($cd);
+	 for ($i = 1; $i <=strlen($cd) ; $i++) {
+    echo(charAt($cd, $i));
+	}
+
+
+
 	 $sellodigital='22fggfgfg';
 	 $sentencia="INSERT INTO tbl_facturas(rfc,monto,razonsocial,tid,tipopago,direccion,ciudad,fecha,sellodigital,iva,facturaexiste) VALUES ('".$rfc."',".$monto.",'".$rsocial."',".$tid.",'".$tpago."','".$direccion."','".$ciudad."','".$fecha."','".$sellodigital."',".$iva.",1)";
-	 	var_dump($sentencia);
-	 	die();
                     if ($conexion->query($sentencia) === TRUE) {
                     	echo "<script language='javascript'>"; 
                             echo "alert('Factura Realizada Corectamente');";
