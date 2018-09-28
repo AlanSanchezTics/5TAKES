60fcafe95d2410e4039eb1753fd0cd753b436c00
alexo111z <33468026+alexo111z@users.noreply.github.com>
2018-09-28 05:57:35 -0500

    avance

---
+++ servicioproveedor.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Registro concluido</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container-fluid">
+<div class="row">
+    <div class="col" style="background-color:lavender; text-align:center;">
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+    <div class="form-group">
+    <img src="logo.png" alt="Smiley face" width="30%" height="30%" class="logoprincipal">
+    </div>
+
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <label class="input-group-text" for="inputGroupSelect01">Opciones</label>
+        </div>
+        <select class="custom-select" id="inputGroupSelect01">
+        <option selected>Selecciona...</option>
+        <option value="1">ServicePACK1</option>
+        <option value="2">ServicePACK2</option>
+        <option value="3">ServicePACK3</option>
+        </select>
+    </div>
+
+    <div class="form-group">
+    <a href="subirdocs.php">
+    <button type="button" class="btn btn-success">Siguiente</button>
+    </a>
+    </div>
+
+    </div>
+    </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ subirdocs.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>BancoPreguntas</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+</head>
+<body>
+<table>
+  <tr>
+    <th><img src="logo.png" alt="Smiley face" width="64" height="64" class="logoprincipal"></th>
+    <th><div class = "lbl">
+<label >Usuario: </label>
+  <label>Default</label>
+  <br>
+  <a href ="" >Cerrar Sesion</a>
+</div></th>
+  </tr>
+</table>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+
+    </ul>
+  </div>
+</nav>
+    <div class="container">
+    
+    <div class="input-group">
+    <div class="custom-file">
+    <input type="file" class="custom-file-input" id="inputGroupFile04">
+    <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+    </div>
+    <div class="input-group-append">
+    <button class="btn btn-outline-secondary" type="button">Button</button>
+    </div>
+    </div>
+        
+    </div>
+</body>
+</html>
