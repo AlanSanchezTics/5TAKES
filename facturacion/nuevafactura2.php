cab1b15e693a9a7013708c8bb2ed9eaf6dfd392f
hectorjpalomino <33034249+hectorjpalomino@users.noreply.github.com>
2018-09-28 01:57:16 -0500

    d

--- facturacion/nuevafactura2.php
+++ facturacion/nuevafactura2.php
@@ -2,17 +2,15 @@
 include '../conexion.php';
 session_start();
 $_POST['tid']=$_SESSION['tid'];
-var_dump($_POST['tid']);
-die();
+Nuevafactura($_POST['rfc'], $_POST['monto'], $_POST['rsocial'],$_POST['tpago'],$_POST['direccion'],$_POST['ciudad'],$_POST['fecha'],$_POST['iva'],$_POST['tid']);
 
-Nuevafactura($_POST['rfc'], $_POST['monto'], $_POST['rsocial'],$_POST['tpago'],$_POST['direccion'],$_POST['ciudad'],$_POST['fecha'],$_POST['rsocial'],$_POST['iva'],$_POST['tid']);
-
-function Nuevafactura($rfc,$monto,$rsocial,$tpago,$direccion,$ciudad,$fecha,$rsocial,$iva,$tid)
+function Nuevafactura($rfc,$monto,$rsocial,$tpago,$direccion,$ciudad,$fecha,$iva,$tid)
 {
 	 include '../conexion.php';
 	 $sellodigital='22fggfgfg';
 	 $sentencia="INSERT INTO tbl_facturas(rfc,monto,razonsocial,tid,tipopago,direccion,ciudad,fecha,sellodigital,iva,facturaexiste) VALUES ('".$rfc."',".$monto.",'".$rsocial."',".$tid.",'".$tpago."','".$direccion."','".$ciudad."','".$fecha."','".$sellodigital."',".$iva.",1)";
-
+	 	var_dump($sentencia);
+	 	die();
                     if ($conexion->query($sentencia) === TRUE) {
                     	echo "<script language='javascript'>"; 
                             echo "alert('Factura Realizada Corectamente');";
