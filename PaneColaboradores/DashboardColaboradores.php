b71a60e0fdf104529825636f01eddb33226696e9
joshua1704 <33034505+joshua1704@users.noreply.github.com>
2018-09-28 12:02:03 -0500

    nuevo

---
+++ PaneColaboradores/DashboardColaboradores.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Dashboard</title>
+  <meta charset="utf-8">
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+  <link href="../css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<table>
+  <tr>
+    <th><img src="../logo.png" alt="Smiley face" width="64" height="64" class="logoprincipal"></th>
+    <th><div class = "lbl">
+  <!--<a href="#"><?php session_start(); echo $_SESSION["usuNombre"];?></a>-->
+  <a href="">Default</a>
+  <br>
+  <a href ="" >Cerrar Sesion</a>
+</div></th>
+  </tr>
+</table>
+
+<!--Hola-->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+    <li><a href="solicitudes.php" >Historial</a></li>
+      <li><a href="finanzas.php">Buscar Proveedores Disponibles</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="container">
+    
+     
+</div>
+
+</body>
+</html>
+
