f875ba264b46868c91a81987af27db7fea083c59
alexo111z <33468026+alexo111z@users.noreply.github.com>
2018-09-28 10:23:39 -0500

    quiz

---
+++ Quiz.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Proveedor</title>
+  <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>5TAKES | Sube tus documentos.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+</head>
+<body>
+
+<div class="row">
+    <div class="col">
+        <img src="logo.png" alt="Smiley face" width="64" height="64" class="logoprincipal">
+    </div>
+    <div class="col" style="text-align:right">
+    <div class="lbl">   
+            <a href ="#" >Cerrar Sesion</a>
+    </div>
+    </div>
+</div>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+
+    </ul>
+  </div>
+</nav>
+
+    <div class="container">
+        
+        <form>
+        <div style="text-align:center">
+            <label>Antes de empezar la búsqueda de empleo he reflexionado y tengo claros cuáles son mis puntos fuertes y débiles.</label><br>
+            <label><input type="radio" name="p1" >Si</label>
+            <label><input type="radio" name="p1" >No</label>
+            <br>
+            <br>
+            <label>Creo que pronto encontraré trabajo.</label><br>
+            <label><input type="radio" name="p2" >Si</label>
+            <label><input type="radio" name="p2" >No</label>
+            <br>
+            <br>
+            <label> Creo que mi apariencia y aspecto (aseo personal, vestimenta, etc.) es el adecuado a los puestos de trabajo a los
+            que aspiro. </label><br>
+            <label><input type="radio" name="p3" >Si</label>
+            <label><input type="radio" name="p3" >No</label>
+            <br>
+            <br>
+            <label>Sé cómo debe ser mi actuación (comportamiento y actitud), en una entrevista de selección. </label><br>
+            <label><input type="radio" name="p4" >Si</label>
+            <label><input type="radio" name="p4" >No</label>
+            <br>
+            <br>
+            <label> Analizo detalladamente el puesto ofertado y busco información sobre la empresa antes de presentarme a una
+            entrevista.</label><br>
+            <label><input type="radio" name="p5" >Si</label>
+            <label><input type="radio" name="p5" >No</label>
+            <br>
+            <br>
+            <label>Creo que la formación es muy importante para conseguir y mantener un puesto de trabajo. </label><br>
+            <label><input type="radio" name="p6" >Si</label>
+            <label><input type="radio" name="p6" >No</label>
+            <br>
+            <br>
+            <label>Creo que mis compañeros/as y amistades me consideran un buen trabajador. </label><br>
+            <label><input type="radio" name="p7" >Si</label>
+            <label><input type="radio" name="p7" >No</label>
+            <br>
+            <br>
+            <label>Me considero un/a buen/a profesional.</label><br>
+            <label><input type="radio" name="p8" >Si</label>
+            <label><input type="radio" name="p8" >No</label>
+            <br>
+            <br>
+            <label>Aceptaría un trabajo para el que tenga que cambiar mi lugar de residencia. </label><br>
+            <label><input type="radio" name="p9" >Si</label>
+            <label><input type="radio" name="p9" >No</label>
+            <br>
+            <br>
+            <label>Aceptaría un trabajo para el que tenga que desplazarme diariamente más de una hora. </label><br>
+            <label><input type="radio" name="p10" >Si</label>
+            <label><input type="radio" name="p10" >No</label>
+            <br>
+            <br>
+
+        </div>
+
+        <div  style="text-align:center">
+            <button type="submit" class="btn btn-success">Subir</button>
+        </div> 
+        <form>      
+    </div>
+
+</body>
+</html>
